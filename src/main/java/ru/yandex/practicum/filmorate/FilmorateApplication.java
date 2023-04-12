28b703dbdf12c31c86916fa70628b75677e33a70
Da4nie-persick <darya.tcheltygmasheva@yandex.ru>
2023-04-12 17:31:47 +0700

    тз9

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,10 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.LocalDate;
-import java.time.Month;
 
 @SpringBootApplication
 public class FilmorateApplication {
